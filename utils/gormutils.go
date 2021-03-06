996f3e152dcc85b0c0c331ce2a19bd7d03955967
王中山 <wangzhognshan@163.com>
2021-03-06 08:58:29 +0800

    完善mysql连接方式的传入

--- utils/gormutils.go
+++ utils/gormutils.go
@@ -2,16 +2,20 @@ package utils
 
 import (
 	"fmt"
+	"github.com/artstylecode/artcoding-go/strings"
 	"github.com/jinzhu/gorm"
 )
 
 func GetDb(configPath string, connectionUrl string) *gorm.DB {
 	configUtils := SysConfig{}
-	configUtils.Load("conf/config.ini")
+	configUtils.Load(configPath)
 	mysqlConfig := configUtils.GetSectionConfig("mysql")
 
 	connectUrl := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
 		mysqlConfig["user"], mysqlConfig["password"], mysqlConfig["host"], mysqlConfig["port"], mysqlConfig["dbName"])
+	if !strings.IsEmptyString(connectionUrl) {
+		connectUrl = connectionUrl
+	}
 	db, err := gorm.Open("mysql", connectUrl)
 	FailOnError(err, "")
 
